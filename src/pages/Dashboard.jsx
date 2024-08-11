606808b15fd7b52cd41182d2f47438f44ff2ed11
muddassiryahya <m.m2442212@gmail.com>
2024-08-11 07:03:56 +0500

    added some line

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,18 +3,49 @@ import AppCard from "../components/ANTcard";
 import { useState } from 'react'
 
 function Dashbord() {
-    console.log("useState--->", useState("Ahmed"));
+    // console.log("useState--->", useState("Ahmed"));
     const [counter, setCounter] = useState(0);
+    const [name, setName] = useState("Maaz");
+
+    const [todos, setTodos] = useState([
+        { value: "Anas" },
+        { value: "Imran" }
+    ]);
+    const [value, setValue] = useState("");
+
+    const addTodo = () => {
+        setTodos([...todos, { value }]);
+        setValue("");
+    }
+
+
     return (
         <div>
+            <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+            <br /> <br />
+
+            <button onClick={addTodo}>Add Todo</button>
+
+            <br /> <br />
+
+            <ul>
+                {todos.map((v, i) => <li key={i}>{v.value}</li>)}
+            </ul>
+            <br />
+
             <AppLayout title="SMIT">
                 <div>
                     <h1>Hello</h1>
                     <AppCard />
                     <h2>{counter}</h2>
                     <button onClick={() => setCounter(counter + 1)}>Increament</button>
+                    <br /> <br />
+                    <h2>{name}</h2>
+                    <button onClick={() => setName("Tamim")}>Update</button>
+                    <br /> <br />
                 </div>
             </AppLayout>
+
         </div>
     )
 }
